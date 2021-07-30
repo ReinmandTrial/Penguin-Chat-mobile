917e99e8f437a22fbe137ef2ad7e0777d0496d97
spjustice <sanyok.vergun@gmail.com>
2021-07-30 12:26:59 +0300

    fixes, add js, 100%

--- app/js/main.js
+++ app/js/main.js
@@ -16,21 +16,59 @@ $('.primary-switcher__item').on('click', function () {
 //popup
 //popup нажатие вне body
 $(document).on('click', function (e) {
-   if (!$(e.target).closest(".delete-dialog").length) {
-      $('.popup').fadeOut();
+   if (!$(e.target).closest(".delete-dialog").length && !$(e.target).closest(".btn-popup").length) {
+      $('.popup').fadeOut('fast');
    }
    e.stopPropagation();
 });
 //popup нажатие вне body end
 
 $('.btn-popup').on('click', function () {
-   var btn = $(this);
-   ; ($('.main').find('.popup')).each(function () {
-      $(this).fadeOut();
-   })
+   $('.main').find('.popup').fadeIn('fast');
 
 })
 $('.popup__close').on('click', function () {
-   $(this).closest('.popup').fadeOut();
+   $(this).closest('.popup').fadeOut('fast');
 })
 //popup end
+
+const buttonItems = document.querySelectorAll('.dialog');
+
+for (let buttonItem of buttonItems) {
+   buttonItem.addEventListener('touchstart', handleTouchStart, false);
+   buttonItem.addEventListener('touchmove', handleTouchMove, false);
+}
+
+var xDown = null;
+var yDown = null;
+
+function getTouches(evt) {
+   return evt.touches || // чистый API JS
+      evt.originalEvent.touches; // jQuery
+}
+
+function handleTouchStart(evt) {
+   const firstTouch = getTouches(evt)[0];
+   xDown = firstTouch.clientX;
+   yDown = firstTouch.clientY;
+};
+
+function handleTouchMove(evt) {
+   if (!xDown || !yDown) {
+      return;
+   }
+
+   var xUp = evt.touches[0].clientX;
+   var yUp = evt.touches[0].clientY;
+
+   var xDiff = xDown - xUp;
+   var yDiff = yDown - yUp;
+   console.log(yDiff)
+   if (xDown < xUp && Math.abs(xDiff) > 2 && Math.abs(yDiff) <= 2) {
+      $(this).removeClass('open')
+   } else if (xDown > xUp && Math.abs(xDiff) > 2 && Math.abs(yDiff) <= 2) {
+      $(this).addClass('open')
+   }
+   /* свайп был, обнуляем координаты */
+   xDown = null;
+};
